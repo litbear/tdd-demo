31b061eb1351b89f8a32f67a2f04e919730a4e10
litbear <litbear@foxmail.com>
2016-10-11 08:39:27 +0800

    5.3 在模版中渲染变量

--- lists/views.py
+++ lists/views.py
@@ -1,8 +1,14 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 
 # Create your views here.
 # 在这儿编写视图
+@csrf_exempt
 def home_page(request):
-    return render(request, 'home.html')
+#     if request.method == 'POST':
+#         return HttpResponse(request.POST['item_text'])
+    return render(request, 'home.html',{
+        'new_item_text': request.POST.get('item_text', ''),
+    })
