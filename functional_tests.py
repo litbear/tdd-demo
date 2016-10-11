effb8113fe2c7e14de41d22797a75da69c67018e
litbear <litbear@foxmail.com>
2016-10-11 12:58:50 +0800

    5.9 使用迁移创建生产数据库

--- functional_tests.py
+++ functional_tests.py
@@ -1,3 +1,4 @@
+import time
 import unittest
 
 from selenium import webdriver
@@ -14,6 +15,8 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
         
     def check_for_row_in_list_table(self, row_text):
+        import time
+        time.sleep(3)
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
@@ -22,6 +25,7 @@ class NewVisitorTest(unittest.TestCase):
         # 伊迪丝听说有一个很酷的在线待办事项应用
         # 她去看了这个应用的首页
         self.browser.get('http://localhost:8000')
+        
         # 她注意到网页的标题和头部都包含 “ To-Do ” 这个词
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
@@ -42,16 +46,6 @@ class NewVisitorTest(unittest.TestCase):
         # 待办事项表格中显示了 “ 1: Buy peacock feathers ”
         
         inputbox.send_keys(Keys.ENTER)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-#         self.assertTrue(
-#             any(row.text == '1: Buy peacock feathers' for row in rows),
-#             "New to-do item did not appear in table -- its text was:\n%s" % (
-#                 table.text,
-#             )
-#         )
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         
         # 页面中又显示了一个文本框，可以输入其他的待办事项
@@ -63,8 +57,6 @@ class NewVisitorTest(unittest.TestCase):
         
         
         # 页面再次更新，她的清单中显示了这两个待办事项
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
         
--- lists/views.py
+++ lists/views.py
@@ -1,14 +1,17 @@
 from django.http.response import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_exempt
+
+from lists.models import Item
 
 
 # Create your views here.
 # 在这儿编写视图
 @csrf_exempt
 def home_page(request):
-#     if request.method == 'POST':
-#         return HttpResponse(request.POST['item_text'])
-    return render(request, 'home.html',{
-        'new_item_text': request.POST.get('item_text', ''),
-    })
+              
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/') 
+    items = Item.objects.all()
+    return render(request, 'home.html', {'items': items})
